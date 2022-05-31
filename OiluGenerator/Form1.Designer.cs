9d42e08334b8acaee81a0c5796af0b0628b77716
fdl <fdl@DESKTOP-B20FLG7>
2022-05-31 15:33:22 +0100

    Ajoutez des fichiers projet.

---
+++ OiluGenerator/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace OiluGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.patternCombo = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.segmentRatioBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ExportBtn = new System.Windows.Forms.Button();
+            this.genBtnn = new System.Windows.Forms.Button();
+            this.sizeCm = new System.Windows.Forms.TextBox();
+            this.resBox = new System.Windows.Forms.TextBox();
+            this.nbrBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.patternCombo);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.segmentRatioBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.ExportBtn);
+            this.panel1.Controls.Add(this.genBtnn);
+            this.panel1.Controls.Add(this.sizeCm);
+            this.panel1.Controls.Add(this.resBox);
+            this.panel1.Controls.Add(this.nbrBox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(341, 707);
+            this.panel1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(117, 256);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Screen resolution";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // patternCombo
+            // 
+            this.patternCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.patternCombo.FormattingEnabled = true;
+            this.patternCombo.Items.AddRange(new object[] {
+            "LINEAR PATTERN",
+            "CIRCULAR PATTERN",
+            "DOTTED PATTERN"});
+            this.patternCombo.Location = new System.Drawing.Point(144, 109);
+            this.patternCombo.Name = "patternCombo";
+            this.patternCombo.Size = new System.Drawing.Size(184, 21);
+            this.patternCombo.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label4.Location = new System.Drawing.Point(13, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Code pattern";
+            // 
+            // segmentRatioBox
+            // 
+            this.segmentRatioBox.Location = new System.Drawing.Point(143, 292);
+            this.segmentRatioBox.Name = "segmentRatioBox";
+            this.segmentRatioBox.Size = new System.Drawing.Size(185, 20);
+            this.segmentRatioBox.TabIndex = 5;
+            this.segmentRatioBox.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label1.Location = new System.Drawing.Point(11, 288);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Segment \r\nBlack/White ratio (%)";
+            // 
+            // ExportBtn
+            // 
+            this.ExportBtn.Enabled = false;
+            this.ExportBtn.Location = new System.Drawing.Point(14, 368);
+            this.ExportBtn.Name = "ExportBtn";
+            this.ExportBtn.Size = new System.Drawing.Size(314, 23);
+            this.ExportBtn.TabIndex = 3;
+            this.ExportBtn.Text = "EXPORT GENERATED CODE";
+            this.ExportBtn.UseVisualStyleBackColor = true;
+            this.ExportBtn.Click += new System.EventHandler(this.ExportBtn_Click);
+            // 
+            // genBtnn
+            // 
+            this.genBtnn.Location = new System.Drawing.Point(15, 339);
+            this.genBtnn.Name = "genBtnn";
+            this.genBtnn.Size = new System.Drawing.Size(314, 23);
+            this.genBtnn.TabIndex = 3;
+            this.genBtnn.Text = "GENERATE";
+            this.genBtnn.UseVisualStyleBackColor = true;
+            this.genBtnn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sizeCm
+            // 
+            this.sizeCm.Location = new System.Drawing.Point(144, 203);
+            this.sizeCm.Name = "sizeCm";
+            this.sizeCm.Size = new System.Drawing.Size(185, 20);
+            this.sizeCm.TabIndex = 2;
+            this.sizeCm.Text = "10";
+            // 
+            // resBox
+            // 
+            this.resBox.Location = new System.Drawing.Point(143, 229);
+            this.resBox.Name = "resBox";
+            this.resBox.Size = new System.Drawing.Size(185, 20);
+            this.resBox.TabIndex = 2;
+            this.resBox.Text = "96";
+            // 
+            // nbrBox
+            // 
+            this.nbrBox.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nbrBox.Location = new System.Drawing.Point(12, 59);
+            this.nbrBox.Name = "nbrBox";
+            this.nbrBox.Size = new System.Drawing.Size(323, 33);
+            this.nbrBox.TabIndex = 1;
+            this.nbrBox.Text = "0000";
+            this.nbrBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nbrBox_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label3.Location = new System.Drawing.Point(13, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Size (cm)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Number to be coded";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label.Location = new System.Drawing.Point(12, 232);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(103, 17);
+            this.label.TabIndex = 0;
+            this.label.Text = "Resolution (dpi)";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(341, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(690, 707);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(690, 707);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(16, 146);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(212, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Inverted (white segments on black bkg)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(16, 169);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(66, 17);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "Oriented";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 707);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Oilu Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox nbrBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sizeCm;
+        private System.Windows.Forms.TextBox resBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button genBtnn;
+        private System.Windows.Forms.Button ExportBtn;
+        private System.Windows.Forms.TextBox segmentRatioBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox patternCombo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ OiluGenerator/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OiluGenerator
+{
+    public partial class Form1 : Form
+    {
+        enum patternCode
+        {
+            LINEAR = 0,
+            CIRCULAR,
+            DOTTED
+        } 
+        
+        private long inputNb;
+        private int sizeInCm;
+        private int resolution;
+        private Bitmap bmp;
+        private float segmentRatio;
+
+        public Form1()
+        {
+            InitializeComponent();
+            patternCombo.SelectedIndex = 0;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputNbStr = nbrBox.Text.Trim();
+
+            if (!long.TryParse(nbrBox.Text, out inputNb))
+            {
+                MessageBox.Show("Invalid input number");
+                return;
+            }
+
+            if (!int.TryParse(sizeCm.Text, out sizeInCm))
+            {
+                MessageBox.Show("Invalid input size");
+                return;
+            }
+
+            if (!int.TryParse(resBox.Text, out resolution))
+            {
+                MessageBox.Show("Invalid input resolution");
+                return;
+            }
+            if (!float.TryParse(segmentRatioBox.Text, out segmentRatio))
+            {
+                MessageBox.Show("Invalid Segments ratio input");
+                return;
+            }
+
+            var pattern = (patternCode) patternCombo.SelectedIndex;
+
+            int idx = inputNbStr.IndexOf('-');
+            if (idx != -1) inputNbStr = inputNbStr.Remove(0, 1);
+
+            int wid = (int)(sizeInCm * resolution / 2.54);
+            int heiht = wid;
+
+            // Set nbLevels per triangle, segment width, ...
+            inputNbStr = "0" + inputNbStr;
+            int nbLevels = inputNbStr.Length  ; 
+            int segmentWidth = (int)((wid / 2.0) / (nbLevels * 2));
+            //segmentRatioBox.Text = segmentRatioBox.Text +" # "+ segmentWidth;
+
+            // create bitmap and grapgics
+
+            bmp = new Bitmap(wid, heiht , System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            bmp.SetResolution(resolution, resolution);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+
+
+            string trLeft   = "";
+            string trBot    = "";
+            string trRight  = "";
+            string trTop    = "";
+
+            
+            for (int i = 0; i < inputNbStr.Length; i++)
+            {
+                byte d = byte.Parse(""+inputNbStr[i]);
+                string dd = oiluToBinarySegments(d);
+                Debug.Assert(!dd.Equals(""));
+
+                trLeft  += dd[0];
+                trBot   += dd[1];
+                trRight += dd[2];
+                trTop   += dd[3];
+
+            }
+
+
+            Bitmap trBmp = null;
+
+            switch (pattern)
+            {
+                case patternCode.LINEAR:
+                    trBmp = GenerateQuaternal(trLeft, segmentWidth, segmentRatio, pattern);
+                    trBmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    g.DrawImage(trBmp, 0, 0);
+
+                    trBmp = GenerateQuaternal(trBot, segmentWidth, segmentRatio, pattern);
+                    trBmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                    g.DrawImage(trBmp, 0, wid / 2);
+
+                    trBmp = GenerateQuaternal(trRight, segmentWidth, segmentRatio, pattern);
+                    trBmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    g.DrawImage(trBmp, wid / 2, 0);
+
+                    trBmp = GenerateQuaternal(trTop, segmentWidth, segmentRatio, pattern);
+                    g.DrawImage(trBmp, 0, 0);
+
+                    break;
+                case patternCode.CIRCULAR:
+                    trBmp = GenerateQuaternal(trLeft, segmentWidth, segmentRatio, pattern);
+                    g.DrawImage(trBmp, 0, 0);
+
+                    trBmp = GenerateQuaternal(trBot, segmentWidth, segmentRatio, pattern);
+                    trBmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                    g.DrawImage(trBmp, 0, heiht / 2);
+
+                    trBmp = GenerateQuaternal(trRight, segmentWidth, segmentRatio, pattern);
+                    trBmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    g.DrawImage(trBmp, wid / 2, heiht / 2);
+
+                    trBmp = GenerateQuaternal(trTop, segmentWidth, segmentRatio, pattern);
+                    trBmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    g.DrawImage(trBmp, wid / 2, 0);
+                    break;
+                case patternCode.DOTTED:
+                    break;
+                default:
+                    break;
+            }
+
+
+            pictureBox1.Image = bmp;
+            pictureBox1.Refresh();
+
+            ExportBtn.Enabled = true;
+
+        }
+
+        private Bitmap GenerateQuaternal(string bits, int segmentWidth, float segmentRatio, patternCode pattern )
+        {
+            switch (pattern)
+            {
+                case patternCode.LINEAR: return GenerateLineraCode(bits, segmentWidth,  segmentRatio);
+                    
+                case patternCode.CIRCULAR: return GenrateCircularCode(bits, segmentWidth,  segmentRatio);
+                case patternCode.DOTTED:return GenerateDottedCode(bits, segmentWidth,  segmentRatio);
+                    ;
+                default: return null;
+                    
+            }
+
+
+        }
+
+        private Bitmap GenerateDottedCode(string bits, int segmentWidth, float segmentRatio)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Bitmap GenrateCircularCode(string bits, int segmentWidth, float segmentRatio)
+        {
+            Bitmap tmpbmp = bmp.Clone(new Rectangle(0, 0, bmp.Width/2, bmp.Height/2 ), bmp.PixelFormat);
+            Graphics gr = Graphics.FromImage(tmpbmp);
+            gr.Clear(Color.White);
+            Pen pen = new Pen(Color.Black, segmentWidth);
+
+            float yy = pen.Width / 2;
+
+            // the first pattern is the square delimiter
+            gr.DrawLine(pen, 0, yy, tmpbmp.Width, yy);
+            gr.DrawLine(pen, yy, 0, yy, tmpbmp.Height); gr.DrawLine(pen, 0, yy, tmpbmp.Width, yy);
+
+
+            yy += 2 * segmentWidth;
+
+            // start from 1
+            for (int i = 1; i < bits.Length; i++)
+            {
+                if (bits[i] == '1')
+                    gr.DrawArc(pen, yy ,yy, 2*tmpbmp.Width -2*yy, 2*tmpbmp.Width -2*yy, 0, 360);
+                yy += 2 * segmentWidth;
+            }
+
+
+
+            tmpbmp.MakeTransparent(Color.White);
+            tmpbmp.SetResolution(bmp.HorizontalResolution, bmp.VerticalResolution);
+            return tmpbmp;
+        }
+
+        private Bitmap GenerateLineraCode(string bits, int segmentWidth, float segmentRatio)
+        {
+            Bitmap tmpbmp = bmp.Clone(new Rectangle(0, 0, bmp.Width, bmp.Height / 2), bmp.PixelFormat);
+            Graphics gr = Graphics.FromImage(tmpbmp);
+            gr.Clear(Color.White);
+            int bsegW = (int)(segmentWidth * segmentRatio / 100);
+            Pen pen = new Pen(Color.Black, bsegW);
+
+            int yy = 0;
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (bits[i] == '1')
+                    gr.DrawLine(pen, 0, yy + pen.Width / 2, tmpbmp.Width, yy + pen.Width / 2);
+                yy += 2 * segmentWidth;
+            }
+
+            Brush br = Brushes.White;
+            Point[] tr = new Point[3] { new Point(0, 0), new Point(0, tmpbmp.Height), new Point(tmpbmp.Width / 2, tmpbmp.Height) };
+            gr.FillPolygon(br, tr);
+
+            tr = new Point[3] { new Point(tmpbmp.Width, 0), new Point(tmpbmp.Width / 2, tmpbmp.Height), new Point(tmpbmp.Width, tmpbmp.Height) };
+            gr.FillPolygon(br, tr);
+
+            tmpbmp.MakeTransparent(Color.White);
+            tmpbmp.SetResolution(bmp.HorizontalResolution, bmp.VerticalResolution);
+            return tmpbmp;
+        }
+
+        private void DrawSegment(Graphics gr, Pen pen, int yy)
+        {
+
+        }
+
+        string oiluToBinarySegments(byte digit)
+        {
+
+            switch (digit)
+            {
+                case 0:
+                    return "1111";
+                case 1:         
+                    return "1000";
+                case 2:         
+                    return "1100";
+                case 3:         
+                    return "1110";
+                case 4:         
+                    return "0110";
+                case 5:         
+                    return "0111";
+                case 6:         
+                    return "0011";
+                case 7:         
+                    return "1011";
+                case 8:         
+                    return "1001";
+                case 9:         
+                    return "1101";
+                default:
+                    return "";
+            }
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (bmp == null) return;
+            SaveFileDialog fd = new SaveFileDialog();
+            fd.Filter = "tif|*.tiff";
+            if(fd.ShowDialog() == DialogResult.OK)
+            {
+                bmp.Save(fd.FileName);
+            }
+        }
+
+        private void nbrBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                genBtnn.PerformClick();
+
+        }
+
+        private void ExportBtn_Click(object sender, EventArgs e)
+        {
+            pictureBox1_DoubleClick(this, null);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if((sender as CheckBox).Checked)
+            {
+                System.Windows.Forms.Screen screen;
+                //Rectangle rect = screen.Primary.
+            }
+        }
+
+    }
+}
